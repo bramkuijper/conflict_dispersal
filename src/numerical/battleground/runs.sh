7ca4e6c9bb632a41fc97e7845275a91cdecfd8cf
Kuijper <alwk201@carson.cm.cluster>
2016-07-20 16:43:20 +0100

    small changes to the compilation of things...

--- src/numerical/battleground/runs.sh
+++ src/numerical/battleground/runs.sh
@@ -1,432 +1,128 @@
 echo 0
-./xconflict_numerical  0.5 2 0 0 0
-echo 1
-./xconflict_numerical  0.5 2 0.05 0 0
-echo 2
-./xconflict_numerical  0.5 2 0.25 0 0
-echo 3
-./xconflict_numerical  0.5 2 0.5 0 0
-echo 4
-./xconflict_numerical  0.5 2 0.75 0 0
-echo 5
-./xconflict_numerical  0.5 2 1.0 0 0
-echo 6
-./xconflict_numerical  0.5 2 0 0 0.05
-echo 7
-./xconflict_numerical  0.5 2 0.05 0 0.05
-echo 8
-./xconflict_numerical  0.5 2 0.25 0 0.05
-echo 9
-./xconflict_numerical  0.5 2 0.5 0 0.05
-echo 10
-./xconflict_numerical  0.5 2 0.75 0 0.05
-echo 11
-./xconflict_numerical  0.5 2 1.0 0 0.05
-echo 12
-./xconflict_numerical  0.5 2 0 0 0.25
-echo 13
-./xconflict_numerical  0.5 2 0.05 0 0.25
-echo 14
-./xconflict_numerical  0.5 2 0.25 0 0.25
-echo 15
-./xconflict_numerical  0.5 2 0.5 0 0.25
-echo 16
-./xconflict_numerical  0.5 2 0.75 0 0.25
-echo 17
-./xconflict_numerical  0.5 2 1.0 0 0.25
-echo 18
-./xconflict_numerical  0.5 2 0 0 0.5
-echo 19
-./xconflict_numerical  0.5 2 0.05 0 0.5
-echo 20
-./xconflict_numerical  0.5 2 0.25 0 0.5
-echo 21
-./xconflict_numerical  0.5 2 0.5 0 0.5
-echo 22
-./xconflict_numerical  0.5 2 0.75 0 0.5
-echo 23
-./xconflict_numerical  0.5 2 1.0 0 0.5
-echo 24
-./xconflict_numerical  0.5 2 0 0 0.75
-echo 25
-./xconflict_numerical  0.5 2 0.05 0 0.75
-echo 26
-./xconflict_numerical  0.5 2 0.25 0 0.75
-echo 27
-./xconflict_numerical  0.5 2 0.5 0 0.75
-echo 28
-./xconflict_numerical  0.5 2 0.75 0 0.75
-echo 29
-./xconflict_numerical  0.5 2 1.0 0 0.75
-echo 30
-./xconflict_numerical  0.5 2 0 0 1.0
-echo 31
-./xconflict_numerical  0.5 2 0.05 0 1.0
-echo 32
-./xconflict_numerical  0.5 2 0.25 0 1.0
-echo 33
-./xconflict_numerical  0.5 2 0.5 0 1.0
-echo 34
-./xconflict_numerical  0.5 2 0.75 0 1.0
-echo 35
-./xconflict_numerical  0.5 2 1.0 0 1.0
-echo 36
-./xconflict_numerical  0.5 2 0 0.05 0
-echo 37
-./xconflict_numerical  0.5 2 0.05 0.05 0
-echo 38
-./xconflict_numerical  0.5 2 0.25 0.05 0
-echo 39
-./xconflict_numerical  0.5 2 0.5 0.05 0
-echo 40
-./xconflict_numerical  0.5 2 0.75 0.05 0
-echo 41
-./xconflict_numerical  0.5 2 1.0 0.05 0
-echo 42
-./xconflict_numerical  0.5 2 0 0.05 0.05
-echo 43
 ./xconflict_numerical  0.5 2 0.05 0.05 0.05
-echo 44
+echo 1
 ./xconflict_numerical  0.5 2 0.25 0.05 0.05
-echo 45
+echo 2
 ./xconflict_numerical  0.5 2 0.5 0.05 0.05
-echo 46
+echo 3
 ./xconflict_numerical  0.5 2 0.75 0.05 0.05
-echo 47
-./xconflict_numerical  0.5 2 1.0 0.05 0.05
-echo 48
-./xconflict_numerical  0.5 2 0 0.05 0.25
-echo 49
+echo 4
 ./xconflict_numerical  0.5 2 0.05 0.05 0.25
-echo 50
+echo 5
 ./xconflict_numerical  0.5 2 0.25 0.05 0.25
-echo 51
+echo 6
 ./xconflict_numerical  0.5 2 0.5 0.05 0.25
-echo 52
+echo 7
 ./xconflict_numerical  0.5 2 0.75 0.05 0.25
-echo 53
-./xconflict_numerical  0.5 2 1.0 0.05 0.25
-echo 54
-./xconflict_numerical  0.5 2 0 0.05 0.5
-echo 55
+echo 8
 ./xconflict_numerical  0.5 2 0.05 0.05 0.5
-echo 56
+echo 9
 ./xconflict_numerical  0.5 2 0.25 0.05 0.5
-echo 57
+echo 10
 ./xconflict_numerical  0.5 2 0.5 0.05 0.5
-echo 58
+echo 11
 ./xconflict_numerical  0.5 2 0.75 0.05 0.5
-echo 59
-./xconflict_numerical  0.5 2 1.0 0.05 0.5
-echo 60
-./xconflict_numerical  0.5 2 0 0.05 0.75
-echo 61
+echo 12
 ./xconflict_numerical  0.5 2 0.05 0.05 0.75
-echo 62
+echo 13
 ./xconflict_numerical  0.5 2 0.25 0.05 0.75
-echo 63
+echo 14
 ./xconflict_numerical  0.5 2 0.5 0.05 0.75
-echo 64
+echo 15
 ./xconflict_numerical  0.5 2 0.75 0.05 0.75
-echo 65
-./xconflict_numerical  0.5 2 1.0 0.05 0.75
-echo 66
-./xconflict_numerical  0.5 2 0 0.05 1.0
-echo 67
-./xconflict_numerical  0.5 2 0.05 0.05 1.0
-echo 68
-./xconflict_numerical  0.5 2 0.25 0.05 1.0
-echo 69
-./xconflict_numerical  0.5 2 0.5 0.05 1.0
-echo 70
-./xconflict_numerical  0.5 2 0.75 0.05 1.0
-echo 71
-./xconflict_numerical  0.5 2 1.0 0.05 1.0
-echo 72
-./xconflict_numerical  0.5 2 0 0.25 0
-echo 73
-./xconflict_numerical  0.5 2 0.05 0.25 0
-echo 74
-./xconflict_numerical  0.5 2 0.25 0.25 0
-echo 75
-./xconflict_numerical  0.5 2 0.5 0.25 0
-echo 76
-./xconflict_numerical  0.5 2 0.75 0.25 0
-echo 77
-./xconflict_numerical  0.5 2 1.0 0.25 0
-echo 78
-./xconflict_numerical  0.5 2 0 0.25 0.05
-echo 79
+echo 16
 ./xconflict_numerical  0.5 2 0.05 0.25 0.05
-echo 80
+echo 17
 ./xconflict_numerical  0.5 2 0.25 0.25 0.05
-echo 81
+echo 18
 ./xconflict_numerical  0.5 2 0.5 0.25 0.05
-echo 82
+echo 19
 ./xconflict_numerical  0.5 2 0.75 0.25 0.05
-echo 83
-./xconflict_numerical  0.5 2 1.0 0.25 0.05
-echo 84
-./xconflict_numerical  0.5 2 0 0.25 0.25
-echo 85
+echo 20
 ./xconflict_numerical  0.5 2 0.05 0.25 0.25
-echo 86
+echo 21
 ./xconflict_numerical  0.5 2 0.25 0.25 0.25
-echo 87
+echo 22
 ./xconflict_numerical  0.5 2 0.5 0.25 0.25
-echo 88
+echo 23
 ./xconflict_numerical  0.5 2 0.75 0.25 0.25
-echo 89
-./xconflict_numerical  0.5 2 1.0 0.25 0.25
-echo 90
-./xconflict_numerical  0.5 2 0 0.25 0.5
-echo 91
+echo 24
 ./xconflict_numerical  0.5 2 0.05 0.25 0.5
-echo 92
+echo 25
 ./xconflict_numerical  0.5 2 0.25 0.25 0.5
-echo 93
+echo 26
 ./xconflict_numerical  0.5 2 0.5 0.25 0.5
-echo 94
+echo 27
 ./xconflict_numerical  0.5 2 0.75 0.25 0.5
-echo 95
-./xconflict_numerical  0.5 2 1.0 0.25 0.5
-echo 96
-./xconflict_numerical  0.5 2 0 0.25 0.75
-echo 97
+echo 28
 ./xconflict_numerical  0.5 2 0.05 0.25 0.75
-echo 98
+echo 29
 ./xconflict_numerical  0.5 2 0.25 0.25 0.75
-echo 99
+echo 30
 ./xconflict_numerical  0.5 2 0.5 0.25 0.75
-echo 100
+echo 31
 ./xconflict_numerical  0.5 2 0.75 0.25 0.75
-echo 101
-./xconflict_numerical  0.5 2 1.0 0.25 0.75
-echo 102
-./xconflict_numerical  0.5 2 0 0.25 1.0
-echo 103
-./xconflict_numerical  0.5 2 0.05 0.25 1.0
-echo 104
-./xconflict_numerical  0.5 2 0.25 0.25 1.0
-echo 105
-./xconflict_numerical  0.5 2 0.5 0.25 1.0
-echo 106
-./xconflict_numerical  0.5 2 0.75 0.25 1.0
-echo 107
-./xconflict_numerical  0.5 2 1.0 0.25 1.0
-echo 108
-./xconflict_numerical  0.5 2 0 0.5 0
-echo 109
-./xconflict_numerical  0.5 2 0.05 0.5 0
-echo 110
-./xconflict_numerical  0.5 2 0.25 0.5 0
-echo 111
-./xconflict_numerical  0.5 2 0.5 0.5 0
-echo 112
-./xconflict_numerical  0.5 2 0.75 0.5 0
-echo 113
-./xconflict_numerical  0.5 2 1.0 0.5 0
-echo 114
-./xconflict_numerical  0.5 2 0 0.5 0.05
-echo 115
+echo 32
 ./xconflict_numerical  0.5 2 0.05 0.5 0.05
-echo 116
+echo 33
 ./xconflict_numerical  0.5 2 0.25 0.5 0.05
-echo 117
+echo 34
 ./xconflict_numerical  0.5 2 0.5 0.5 0.05
-echo 118
+echo 35
 ./xconflict_numerical  0.5 2 0.75 0.5 0.05
-echo 119
-./xconflict_numerical  0.5 2 1.0 0.5 0.05
-echo 120
-./xconflict_numerical  0.5 2 0 0.5 0.25
-echo 121
+echo 36
 ./xconflict_numerical  0.5 2 0.05 0.5 0.25
-echo 122
+echo 37
 ./xconflict_numerical  0.5 2 0.25 0.5 0.25
-echo 123
+echo 38
 ./xconflict_numerical  0.5 2 0.5 0.5 0.25
-echo 124
+echo 39
 ./xconflict_numerical  0.5 2 0.75 0.5 0.25
-echo 125
-./xconflict_numerical  0.5 2 1.0 0.5 0.25
-echo 126
-./xconflict_numerical  0.5 2 0 0.5 0.5
-echo 127
+echo 40
 ./xconflict_numerical  0.5 2 0.05 0.5 0.5
-echo 128
+echo 41
 ./xconflict_numerical  0.5 2 0.25 0.5 0.5
-echo 129
+echo 42
 ./xconflict_numerical  0.5 2 0.5 0.5 0.5
-echo 130
+echo 43
 ./xconflict_numerical  0.5 2 0.75 0.5 0.5
-echo 131
-./xconflict_numerical  0.5 2 1.0 0.5 0.5
-echo 132
-./xconflict_numerical  0.5 2 0 0.5 0.75
-echo 133
+echo 44
 ./xconflict_numerical  0.5 2 0.05 0.5 0.75
-echo 134
+echo 45
 ./xconflict_numerical  0.5 2 0.25 0.5 0.75
-echo 135
+echo 46
 ./xconflict_numerical  0.5 2 0.5 0.5 0.75
-echo 136
+echo 47
 ./xconflict_numerical  0.5 2 0.75 0.5 0.75
-echo 137
-./xconflict_numerical  0.5 2 1.0 0.5 0.75
-echo 138
-./xconflict_numerical  0.5 2 0 0.5 1.0
-echo 139
-./xconflict_numerical  0.5 2 0.05 0.5 1.0
-echo 140
-./xconflict_numerical  0.5 2 0.25 0.5 1.0
-echo 141
-./xconflict_numerical  0.5 2 0.5 0.5 1.0
-echo 142
-./xconflict_numerical  0.5 2 0.75 0.5 1.0
-echo 143
-./xconflict_numerical  0.5 2 1.0 0.5 1.0
-echo 144
-./xconflict_numerical  0.5 2 0 0.75 0
-echo 145
-./xconflict_numerical  0.5 2 0.05 0.75 0
-echo 146
-./xconflict_numerical  0.5 2 0.25 0.75 0
-echo 147
-./xconflict_numerical  0.5 2 0.5 0.75 0
-echo 148
-./xconflict_numerical  0.5 2 0.75 0.75 0
-echo 149
-./xconflict_numerical  0.5 2 1.0 0.75 0
-echo 150
-./xconflict_numerical  0.5 2 0 0.75 0.05
-echo 151
+echo 48
 ./xconflict_numerical  0.5 2 0.05 0.75 0.05
-echo 152
+echo 49
 ./xconflict_numerical  0.5 2 0.25 0.75 0.05
-echo 153
+echo 50
 ./xconflict_numerical  0.5 2 0.5 0.75 0.05
-echo 154
+echo 51
 ./xconflict_numerical  0.5 2 0.75 0.75 0.05
-echo 155
-./xconflict_numerical  0.5 2 1.0 0.75 0.05
-echo 156
-./xconflict_numerical  0.5 2 0 0.75 0.25
-echo 157
+echo 52
 ./xconflict_numerical  0.5 2 0.05 0.75 0.25
-echo 158
+echo 53
 ./xconflict_numerical  0.5 2 0.25 0.75 0.25
-echo 159
+echo 54
 ./xconflict_numerical  0.5 2 0.5 0.75 0.25
-echo 160
+echo 55
 ./xconflict_numerical  0.5 2 0.75 0.75 0.25
-echo 161
-./xconflict_numerical  0.5 2 1.0 0.75 0.25
-echo 162
-./xconflict_numerical  0.5 2 0 0.75 0.5
-echo 163
+echo 56
 ./xconflict_numerical  0.5 2 0.05 0.75 0.5
-echo 164
+echo 57
 ./xconflict_numerical  0.5 2 0.25 0.75 0.5
-echo 165
+echo 58
 ./xconflict_numerical  0.5 2 0.5 0.75 0.5
-echo 166
+echo 59
 ./xconflict_numerical  0.5 2 0.75 0.75 0.5
-echo 167
-./xconflict_numerical  0.5 2 1.0 0.75 0.5
-echo 168
-./xconflict_numerical  0.5 2 0 0.75 0.75
-echo 169
+echo 60
 ./xconflict_numerical  0.5 2 0.05 0.75 0.75
-echo 170
+echo 61
 ./xconflict_numerical  0.5 2 0.25 0.75 0.75
-echo 171
+echo 62
 ./xconflict_numerical  0.5 2 0.5 0.75 0.75
-echo 172
+echo 63
 ./xconflict_numerical  0.5 2 0.75 0.75 0.75
-echo 173
-./xconflict_numerical  0.5 2 1.0 0.75 0.75
-echo 174
-./xconflict_numerical  0.5 2 0 0.75 1.0
-echo 175
-./xconflict_numerical  0.5 2 0.05 0.75 1.0
-echo 176
-./xconflict_numerical  0.5 2 0.25 0.75 1.0
-echo 177
-./xconflict_numerical  0.5 2 0.5 0.75 1.0
-echo 178
-./xconflict_numerical  0.5 2 0.75 0.75 1.0
-echo 179
-./xconflict_numerical  0.5 2 1.0 0.75 1.0
-echo 180
-./xconflict_numerical  0.5 2 0 1.0 0
-echo 181
-./xconflict_numerical  0.5 2 0.05 1.0 0
-echo 182
-./xconflict_numerical  0.5 2 0.25 1.0 0
-echo 183
-./xconflict_numerical  0.5 2 0.5 1.0 0
-echo 184
-./xconflict_numerical  0.5 2 0.75 1.0 0
-echo 185
-./xconflict_numerical  0.5 2 1.0 1.0 0
-echo 186
-./xconflict_numerical  0.5 2 0 1.0 0.05
-echo 187
-./xconflict_numerical  0.5 2 0.05 1.0 0.05
-echo 188
-./xconflict_numerical  0.5 2 0.25 1.0 0.05
-echo 189
-./xconflict_numerical  0.5 2 0.5 1.0 0.05
-echo 190
-./xconflict_numerical  0.5 2 0.75 1.0 0.05
-echo 191
-./xconflict_numerical  0.5 2 1.0 1.0 0.05
-echo 192
-./xconflict_numerical  0.5 2 0 1.0 0.25
-echo 193
-./xconflict_numerical  0.5 2 0.05 1.0 0.25
-echo 194
-./xconflict_numerical  0.5 2 0.25 1.0 0.25
-echo 195
-./xconflict_numerical  0.5 2 0.5 1.0 0.25
-echo 196
-./xconflict_numerical  0.5 2 0.75 1.0 0.25
-echo 197
-./xconflict_numerical  0.5 2 1.0 1.0 0.25
-echo 198
-./xconflict_numerical  0.5 2 0 1.0 0.5
-echo 199
-./xconflict_numerical  0.5 2 0.05 1.0 0.5
-echo 200
-./xconflict_numerical  0.5 2 0.25 1.0 0.5
-echo 201
-./xconflict_numerical  0.5 2 0.5 1.0 0.5
-echo 202
-./xconflict_numerical  0.5 2 0.75 1.0 0.5
-echo 203
-./xconflict_numerical  0.5 2 1.0 1.0 0.5
-echo 204
-./xconflict_numerical  0.5 2 0 1.0 0.75
-echo 205
-./xconflict_numerical  0.5 2 0.05 1.0 0.75
-echo 206
-./xconflict_numerical  0.5 2 0.25 1.0 0.75
-echo 207
-./xconflict_numerical  0.5 2 0.5 1.0 0.75
-echo 208
-./xconflict_numerical  0.5 2 0.75 1.0 0.75
-echo 209
-./xconflict_numerical  0.5 2 1.0 1.0 0.75
-echo 210
-./xconflict_numerical  0.5 2 0 1.0 1.0
-echo 211
-./xconflict_numerical  0.5 2 0.05 1.0 1.0
-echo 212
-./xconflict_numerical  0.5 2 0.25 1.0 1.0
-echo 213
-./xconflict_numerical  0.5 2 0.5 1.0 1.0
-echo 214
-./xconflict_numerical  0.5 2 0.75 1.0 1.0
-echo 215
-./xconflict_numerical  0.5 2 1.0 1.0 1.0
